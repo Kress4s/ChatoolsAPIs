51b019556ef971bbcb9a03e8872c5be08eef7d3b
York-xia <1208209000@qq.com>
2020-12-09 22:21:50 +0800

    修改协议地址

--- app/bridage/constant/const.go
+++ app/bridage/constant/const.go
@@ -29,7 +29,7 @@ const (
 )
 
 const (
-	BASE_URL        = "http://49.234.86.244:8080" //微信对接baselink
+	BASE_URL        = "http://49.234.86.244:8090" //微信对接baselink
 	GRPC_BASE_URL   = "49.234.86.244:8081"        // 消息监控的地址
 	GRPC_RUN_SERVER = "139.159.137.37:9000"
 )
